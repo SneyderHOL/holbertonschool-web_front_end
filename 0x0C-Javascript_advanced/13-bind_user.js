04eb1a3c44628a21dbef8a6309fdf0244b0248fd
SneyderHOL <1507@holbertonschool.com>
2021-04-02 15:20:35 -0500

    [FIX]: output messages

--- 0x0C-Javascript_advanced/13-bind_user.js
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -11,4 +11,4 @@ function logWelcomeUser (welcomeString) {
   console.log(`${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`);
 }
 const bindLogWelcomeUser = logWelcomeUser.bind(user);
-logWelcomeUser('Welcome');
+bindLogWelcomeUser('Welcome');
--- 0x0C-Javascript_advanced/8-payments.js
+++ 0x0C-Javascript_advanced/8-payments.js
@@ -6,6 +6,7 @@ function processOrder (orderId, amount) {
   processPayment(amount);
   console.log(`${orderId} has been fully processed`);
 }
+console.log('Processing orders');
 processOrder(12321, 10.99);
 processOrder(12322, 12.99);
 processOrder(12323, 15.0);
